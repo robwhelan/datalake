cf191e25c7c04f3a82a8830c491bd75a5d2088cb
Rob Whelan <whelan@gmail.com>
2019-08-22 16:20:57 -0500

    remove logging bucket from template

--- create-datalake.sh
+++ create-datalake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 aws cloudformation create-stack --stack-name $1 \
-  --template-url $2
-  --parameters file://main-template-parameters.json
+  --template-url $2 \
+  --parameters file://main-template-parameters.json \
+  --capabilities CAPABILITY_NAMED_IAM
