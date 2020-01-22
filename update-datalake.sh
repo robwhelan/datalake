0bf21b5514cc63c0d858882c9b350aad3ed0ae37
Rob Whelan <whelan@gmail.com>
2020-01-22 13:24:43 -0600

    allow for easy stack update

--- update-datalake.sh
+++ update-datalake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 aws cloudformation update-stack --stack-name $1 \
   --template-url $2 \
-  --parameters file://main-template-parameters.json \
+  --parameters \
+    ParameterKey=pS3TemplateLocation,UsePreviousValue=true \
+    ParameterKey=pProjectName,UsePreviousValue=true \
   --capabilities CAPABILITY_NAMED_IAM
